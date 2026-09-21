bb57d6d8c5bf723afd3a0430b0c1feb1affdcc7b
Charan Raj <223257037+CHARANRAJ45@users.noreply.github.com>
2026-09-21 16:45:18 +0000

    Add multi-agent research application

---
+++ pipeline.py
@@ -0,0 +1,67 @@
+from agents import build_reader_agent , build_search_agent , writer_chain , critic_chain
+
+
+def run_research_pipeline(topic: str) -> dict:
+    state ={}
+
+    #search agent 
+    print("\n"+"="*50)
+    print("step -1 search agent is working")
+    print("="*50)
+
+    search_agent = build_search_agent()
+    search_results = search_agent.invoke({
+        "messages": [("user", f"find recent and reliable information about {topic}")]
+    })
+    state["search_results"] = search_results["messages"][-1].content
+    print("\n search result : ",state["search_results"])
+
+#step-2 reader agent
+    print("\n"+"="*50)
+    print("step -2 reader agent is working")
+    print("="*50)
+
+    reader_agent = build_reader_agent()
+    reader_result = reader_agent.invoke({
+         "messages": [("user",
+            f"Based on the following search results about '{topic}', "
+            f"pick the most relevant URL and scrape it for deeper content.\n\n"
+            f"Search Results:\n{state['search_results'][:800]}"
+        )]
+    })
+    state['scraped_content'] = reader_result['messages'][-1].content
+    print("\n scraped content : ",state["scraped_content"])
+
+#step-3 writer agent
+    print("\n"+"="*50)
+    print("step -3 writer agent is working")
+    print("="*50)
+
+    research_combined = (
+        f"search result : \n {state['search_results']}"
+        f"\n\nscraped content : \n {state['scraped_content']}"
+    )
+
+    state['report'] = writer_chain.invoke({
+        "topic": topic,
+        "research": research_combined
+    })
+    print("\n report : ",state["report"])
+
+#step-4 critic agent
+    print("\n"+"="*50)
+    print("step -4 critic agent is working")
+    print("="*50)
+
+    state['feedback'] = critic_chain.invoke({
+        "report": state['report']
+    })
+    print("\n feedback : ",state["feedback"])
+
+
+    return state
+
+if __name__ == "__main__":
+    topic = input("\nEnter a research topic: ")
+    run_research_pipeline(topic)
+    
